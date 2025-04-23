bcaf9d031cef9b88a23f006c1090b71e347035c5
schweppesXP <serdobincevnikita3@gmail.com>
2025-04-23 17:10:32 +0300

    Update app.js

--- app.js
+++ app.js
@@ -126,7 +126,7 @@ function renderMovieCard(movie) {
             <svg class="check-icon" xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round">
               <polyline points="20 6 9 17 4 12"/>
             </svg>
-            Отметить как просмотрено
+            Посмотрел
           </button>
         ` : ''}
       </div>
